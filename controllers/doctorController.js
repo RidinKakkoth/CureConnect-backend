58d1b206aafa34eb1460aae58d593160cd48aa06
Ridin Kakkoth <k.ridin@ymail.com>
2025-02-08 12:25:33 +0530

    last commit

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -126,5 +126,65 @@ const appointmentCancel=async(req,res)=>{
     }
 }
 
+//api to get dashboard data for doctor panel
 
-export { changeAvailability,doctorList,loginDoctor,appointmentsDoctor,appointmentComplete,appointmentCancel };
+const doctorDashboard=async(req,res)=>{
+    try {
+        const docId=req.docId
+        
+        const appointments=await appointmentModel.find({docId})
+
+        let earnings=0
+       await appointments.map((item,index)=>{
+            if(item.isCompleted||item.payment){
+                earnings+=item.amount
+            }
+        })
+        let patients=[]
+       await appointments.map((item,index)=>{
+            if(!patients.includes(item.userId)){
+                patients.push(item.userId)
+            }
+        })
+        const dashData={
+            earnings,
+            appointments:appointments.length,
+            patients:patients.length,
+            latestAppointments:appointments.reverse().slice(0,5)
+        }
+        res.json({success:true,dashData})
+
+    } catch (error) {
+        res.json({success:false,message:error.message})
+    }
+}
+
+//api to get doctor profile for doctor panel
+
+const doctorProfile=async(req,res)=>{
+    try {
+        
+        const docId=req.docId
+        const profileData=await doctorModel.findById(docId).select('-password')
+        res.json({success:true,profileData})
+    } catch (error) {
+       res.json({success:false,message:error.message})
+   }
+}
+//api to update doctor profile for doctor panel
+
+const updateDoctorProfile=async(req,res)=>{
+    try {
+        
+        const docId=req.docId
+        const{fees,address,available}=req.body
+        await doctorModel.findByIdAndUpdate(docId,{fees,address,available})
+        res.json({success:true,message:"Profile Updated"})
+    } catch (error) {
+       res.json({success:false,message:error.message})
+   }
+}
+
+
+
+export { changeAvailability,doctorList,loginDoctor,appointmentsDoctor,appointmentComplete,appointmentCancel,doctorDashboard,updateDoctorProfile,doctorProfile };
